7cc3507e1a0071afd4eae952288d9a5121a94c37
jagadpurnomo <jagad.purnomo@b2be.com>
2024-09-27 15:33:52 +0700

    fix: home card padding

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -5,7 +5,7 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <section class="py-4">
-    <div class="container-xl lg:container m-auto">
+    <div class="container-xl lg:container m-auto px-4">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
         <Card>
           <h2 class="text-2xl font-bold">For Developers</h2>
